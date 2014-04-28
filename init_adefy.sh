afd6801342be614015b3b214d67a0654a7cf811f
Cris Mihalache <cris.mihalache@spectrumit.eu>
2014-04-28 17:49:23 +0200

    Removed profoundgrid, added TT to team page, removed Speed

--- init_adefy.sh
+++ init_adefy.sh
@@ -2,16 +2,18 @@
 #
 # Builds adefy bower components
 # In future versions, this will no longer be necessary
-cd bower_components
+cd _components
 
 cd adefyjs
 npm install
 grunt cdn
+rm -rf node_modules
 cd ..
 
 cd adefyre
 npm install
 grunt cdn
+rm -rf node_modules
 cd ..
 
 cd ..
